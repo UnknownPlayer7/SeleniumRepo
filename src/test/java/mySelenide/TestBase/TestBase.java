0226df456a90cda59754a5f99abfe3478c75b729
Dmitriy Good <goodamov@gmail.com>
2025-11-08 16:23:09 +0300

    add AllureSelenide

--- src/test/java/mySelenide/TestBase/TestBase.java
+++ src/test/java/mySelenide/TestBase/TestBase.java
@@ -2,14 +2,22 @@ package mySelenide.TestBase;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Listeners;
+import org.testng.annotations.BeforeSuite;
 
-@Listeners({ScreenshotListener.class})
 public class TestBase {
+
+    @BeforeSuite
+    public void setUpAllureSelenide() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide()
+                .screenshots(true)
+                .savePageSource(false));
+    }
 
     @BeforeMethod
     public void setup() {
